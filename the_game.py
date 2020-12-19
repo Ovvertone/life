6e0a9417e2d2e5bf4a7c9ece584a1b1c232c6ef6
Vyacheslav <shirkeev@example.com>
2020-12-20 02:55:24 +0700

    Some refactor

--- the_game.py
+++ the_game.py
@@ -1,11 +1,12 @@
 from random import choice, random
+from os import system, name
+
 from colorama import Fore, Style
 import matplotlib.pyplot as plt
-from os import system, name
 from numpy import round
 
 
-def coloring(text, color):
+def coloring(text: str, color: str) -> str:
     colors = {'r': Fore.RED, 'g': Fore.GREEN, 'b': Fore.BLUE, 'c': Fore.LIGHTCYAN_EX, 'w': Fore.WHITE}
     try:
         return colors[color] + text + Style.RESET_ALL
@@ -23,14 +24,14 @@ FOOD = coloring('o', 'g')
 field = [[choice([ALIVE, DEAD]) for x in range(SIZE_X)] for y in range(SIZE_Y)]
 
 
-def get_poison_and_food(field, prob):
+def get_poison_and_food(field: list, prob: float) -> None:
     for x in range(SIZE_X):
         for y in range(SIZE_Y):
             field[y][x] = POISON if random() < prob and field[y][x] == DEAD else field[y][x]
             field[y][x] = FOOD if random() < prob and field[y][x] == DEAD else field[y][x]
 
 
-def scanner(x, y):
+def scanner(x: int, y: int) -> (int, int):
     for dx, dy in (
             (0, 1),
             (1, 1),
@@ -58,38 +59,37 @@ def scanner(x, y):
         yield scan_x, scan_y
 
 
-def get_field(field):
+def get_field(field: list) -> None:
     for y in range(SIZE_Y):
         print(''.join(field[y]))
 
 
-def get_empty_field(field):
+def get_empty_field(field: list) -> list:
     return [[DEAD for x in range(SIZE_X)] for y in range(SIZE_Y)]
 
 
-def is_alive(field, neighbor_x, neighbor_y):
+def is_alive(field: list, neighbor_x: int, neighbor_y: int) -> bool:
     return 0 <= neighbor_x < SIZE_X \
            and 0 <= neighbor_y < SIZE_Y \
            and field[neighbor_y][neighbor_x] == ALIVE
 
 
-def is_poison(field, neighbor_x, neighbor_y):
+def is_poison(field: list, neighbor_x: int, neighbor_y: int) -> bool:
     return 0 <= neighbor_x < SIZE_X \
            and 0 <= neighbor_y < SIZE_Y \
            and field[neighbor_y][neighbor_x] == POISON
 
 
-def is_food(field, neighbor_x, neighbor_y):
+def is_food(field: list, neighbor_x: int, neighbor_y: int) -> bool:
     return 0 <= neighbor_x < SIZE_X \
            and 0 <= neighbor_y < SIZE_Y \
            and field[neighbor_y][neighbor_x] == FOOD
 
 
-alive_list = []
-generation_list = []
+alive_list, generation_list, food_list, poison_list = [], [], [], []
 generation = 1
 while True:
-    alive_count = 0
+    alive_count, food_count, poison_count = 0, 0, 0
     print(f'Generation: {generation}')
     get_poison_and_food(field, .0001)
     get_field(field)
@@ -99,6 +99,8 @@ while True:
         for x in range(SIZE_X):
             cell = field[y][x]
             alive_count += 1 if cell == ALIVE else 0
+            food_count += 1 if cell == FOOD else 0
+            poison_count += 1 if cell == POISON else 0
             neighbors = 0
 
             for neighbor_x, neighbor_y in scanner(x, y):
@@ -124,17 +126,23 @@ while True:
         break
     field = buffer
 
-    generation += 1
     generation_list.append(generation)
     alive_list.append(alive_count)
+    food_list.append(food_count)
+    poison_list.append(poison_count)
+    generation += 1
 
     system('clear' if name != 'nt' else 'cls')
 
 fig, ax = plt.subplots()
-plt.title('Diagram of alive cells count')
-ax.plot(generation_list, [round((i / (SIZE_X * SIZE_Y)) * 100, 2) for i in alive_list], 'g')
+plt.title('Diagram of cells count')
+ax.plot(generation_list, [round((i / (SIZE_X * SIZE_Y)) * 100, 2) for i in alive_list], 'b', label='Alive')
+ax.plot(generation_list, [round((i / (SIZE_X * SIZE_Y)) * 100, 2) for i in food_list], 'g', label='Food')
+ax.plot(generation_list, [round((i / (SIZE_X * SIZE_Y)) * 100, 2) for i in poison_list], 'r', label='Poison')
+ax.legend()
+plt.axis([1, 500, 0, 100])
 ax.set_xlabel('Generation')
-ax.set_ylabel('Count of alive cells (%)')
+ax.set_ylabel('Count of alive cells, food cells and poison cells (%)')
 ax.grid()
 fig.set_figwidth(18)
 fig.set_figheight(9)
